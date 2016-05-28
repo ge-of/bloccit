8c85db0d7d3928074359160f46835ed4d76ce79a
Geoff Miles <miles.geoff@gmail.com>
2016-05-28 15:10:09 -0400

    assignment-28

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,12 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  let(:question) {Question.create!(title: "New Question Title", body: "New Question Body", resolved: true )}
-  let(:answer) {Answer.create!(body: 'Answer Body', question: question) }
+  let(:question) {Question.new(title: "New Question Title", body: "New Question Body", resolved: true )}
+  let(:answer) {Answer.new(body: 'Answer Body', question: question) }
 
-  describe "attributes" do
-    it "has a body attribute" do
-      expect(answer).to have_attributes(body: "Answer Body")
-    end
+  it "should respond to body" do
+    expect(answer).to respond_to(:body)
   end
 end
