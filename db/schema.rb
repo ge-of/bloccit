ac7cdb645afdb6ad2b192d458ecb94d974917a28
Geoff Miles <miles.geoff@gmail.com>
2016-06-09 21:14:02 -0400

    Checkpoint-36

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160528192421) do
+ActiveRecord::Schema.define(version: 20160610004333) do
 
   create_table "adverisements", force: :cascade do |t|
     t.string   "title"
@@ -40,13 +40,16 @@ ActiveRecord::Schema.define(version: 20160528192421) do
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
 
-  create_table "questions", force: :cascade do |t|
+  create_table "sponsored_posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.boolean  "resolved"
+    t.integer  "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "topic_id"
   end
+
+  add_index "sponsored_posts", ["topic_id"], name: "index_sponsored_posts_on_topic_id"
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
@@ -54,6 +57,14 @@ ActiveRecord::Schema.define(version: 20160528192421) do
     t.text     "description"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
