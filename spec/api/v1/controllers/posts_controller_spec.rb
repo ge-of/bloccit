57bd075e506839ea6d0acc06c1702c70726fbdae
Geoff Miles <miles.geoff@gmail.com>
2016-07-31 13:26:25 -0400

    Assignment 49 WIP

--- spec/api/v1/controllers/posts_controller_spec.rb
+++ spec/api/v1/controllers/posts_controller_spec.rb
@@ -1,36 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe Api::V1::TopicsController, type: :controller do
+RSpec.describe Api::V1::PostsController, type: :controller do
   let(:my_user) { create(:user) }
-  let(:my_topic) { create(:topic) }
+  let(:my_post) { create(:post) }
 
-  it { is_expected.to have_many(:posts) }
+  it { is_expected.to belong_to(:topic) }
 
-# #20
   context "unauthenticated user" do
-    it "GET index returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-
-    it "GET show returns http success" do
-      get :show, id: my_topic.id
-      expect(response).to have_http_status(:success)
-    end
-
 
     it "PUT update returns http unauthenticated" do
-      put :update, id: my_topic.id, topic: {name: "Topic Name", description: "Topic Description"}
+      put :update, id: my_post.id, post: {title: "Post Name", body: "Body of the post"}
       expect(response).to have_http_status(401)
     end
 
     it "POST create returns http unauthenticated" do
-      post :create, topic: {name: "Topic Name", description: "Topic Description"}
+      post :create, post: {title: "Post Name", body: "Body of the post"}
       expect(response).to have_http_status(401)
     end
 
     it "DELETE destroy returns http unauthenticated" do
-      delete :destroy, id: my_topic.id
+      delete :destroy, id: my_post.id
       expect(response).to have_http_status(401)
     end
   end
@@ -40,29 +29,18 @@ RSpec.describe Api::V1::TopicsController, type: :controller do
       controller.request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(my_user.auth_token)
     end
 
-# #21
-    it "GET index returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-
-    it "GET show returns http success" do
-      get :show, id: my_topic.id
-      expect(response).to have_http_status(:success)
-    end
-
     it "PUT update returns http forbidden" do
-      put :update, id: my_topic.id, topic: {name: "Topic Name", description: "Topic Description"}
+      put :update, id: my_post.id, post: {title: "Post Name", body: "Body of the post"}
       expect(response).to have_http_status(403)
     end
 
     it "POST create returns http forbidden" do
-      post :create, topic: {name: "Topic Name", description: "Topic Description"}
+      post :create, post: {title: "Post Name", body: "Body of the post"}
       expect(response).to have_http_status(403)
     end
 
     it "DELETE destroy returns http forbidden" do
-      delete :destroy, id: my_topic.id
+      delete :destroy, id: my_post.id
       expect(response).to have_http_status(403)
     end
   end
@@ -72,11 +50,11 @@ RSpec.describe Api::V1::TopicsController, type: :controller do
     before do
       my_user.admin!
       controller.request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(my_user.auth_token)
-      @new_topic = build(:topic)
+      @new_post = build(:post)
     end
 
     describe "PUT update" do
-      before { put :update, id: my_topic.id, topic: {name: @new_topic.name, description: @new_topic.description} }
+      before { put :update, id: my_post.id, post: {title: @new_post.title, body: @new_post.body} }
 
       it "returns http success" do
         expect(response).to have_http_status(:success)
@@ -94,7 +72,7 @@ RSpec.describe Api::V1::TopicsController, type: :controller do
     end
 
     describe "POST create" do
-      before { post :create, topic: {name: @new_topic.name, description: @new_topic.description} }
+      before { post :create, post: {title: @new_post.title, body: @new_post.body} }
 
       it "returns http success" do
         expect(response).to have_http_status(:success)
@@ -106,13 +84,13 @@ RSpec.describe Api::V1::TopicsController, type: :controller do
 
       it "creates a topic with the correct attributes" do
         hashed_json = JSON.parse(response.body)
-        expect(hashed_json["name"]).to eq(@new_topic.name)
-        expect(hashed_json["description"]).to eq(@new_topic.description)
+        expect(hashed_json["name"]).to eq(@new_post.title)
+        expect(hashed_json["description"]).to eq(@new_post.body)
       end
     end
 
     describe "DELETE destroy" do
-      before { delete :destroy, id: my_topic.id }
+      before { delete :destroy, id: my_post.id }
 
     # #18
       it "returns http success" do
@@ -124,12 +102,12 @@ RSpec.describe Api::V1::TopicsController, type: :controller do
       end
 
       it "returns the correct json success message" do
-        expect(response.body).to eq({ message: "Topic destroyed", status: 200 }.to_json)
+        expect(response.body).to eq({ message: "Post deleted", status: 200 }.to_json)
       end
 
-      it "deletes my_topic" do
+      it "deletes my_post" do
     # #19
-        expect{ Topic.find(my_topic.id) }.to raise_exception(ActiveRecord::RecordNotFound)
+        expect{ Post.find(my_post.id) }.to raise_exception(ActiveRecord::RecordNotFound)
       end
     end
   end
