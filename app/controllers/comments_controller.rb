3a56ced2c4d15477862bc9be6c8d93d7cdc115ae
Geoff Miles <miles.geoff@gmail.com>
2016-07-31 21:46:25 -0400

    Assignment 42 WIP; No such column error

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,73 +1,67 @@
 class CommentsController < ApplicationController
  # #10
    before_action :require_sign_in
-
    before_action :authorize_user, only: [:destroy]
-   context
 
    def destroy
-     @post = Post.find(params[:post_id])
-     comment = @post.comments.find(params[:id])
+     if params[:topic_id].present?
+       # comment on a topic
+       @topic = Topic.find(params[:topic_id])
+       comment = @topic.comments.find(params[:id])
 
-     if comment.destroy
-       flash[:notice] = "Comment was deleted successfully."
-       redirect_to [@post.topic, @post]
-     else
-       flash[:alert] = "Comment couldn't be deleted. Try again."
-       redirect_to [@post.topic, @post]
-     end
-   end
+       if comment.destroy
+         flash[:notice] = "Comment was deleted successfully."
+         redirect_to [@topic]
+       else
+         flash[:alert] = "Comment couldn't be deleted. Try again."
+         redirect_to [@topic]
+       end
 
-   def create
- # #11
-     @post = Post.find(params[:post_id])
-     comment = @post.comments.new(comment_params)
-     comment.user = current_user
+     elsif params[:post_id].present?
+       # comment on a post
+       @post = Post.find(params[:post_id])
+       comment = @post.comments.find(params[:id])
 
-     if comment.save
-       flash[:notice] = "Comment saved successfully."
- # #12
-       redirect_to [@post.topic, @post]
-     else
-       flash[:alert] = "Comment failed to save."
- # #13
-       redirect_to [@post.topic, @post]
-     end
-   end
-
-   context
-
-   def destroy
-     @topic = Topic.find(params[:post_id])
-     comment = @topic.comments.find(params[:id])
-
-     if comment.destroy
-       flash[:notice] = "Comment was deleted successfully."
-       redirect_to [@topic]
-     else
-       flash[:alert] = "Comment couldn't be deleted. Try again."
-       redirect_to [@topic]
+       if comment.destroy
+         flash[:notice] = "Comment was deleted successfully."
+         redirect_to [@post.topic, @post]
+       else
+         flash[:alert] = "Comment couldn't be deleted. Try again."
+         redirect_to [@post.topic, @post]
+       end
      end
    end
 
    def create
      if params[:topic_id].present?
        # comment on a topic
+       @topic = Topic.find(params[:topic_id])
+       comment = @topic.comments.new(comment_params)
+       comment.user = current_user
+
+       if comment.save
+         flash[:notice] = "Comment saved successfully."
+         redirect_to [@topic]
+       else
+         flash[:alert] = "Comment failed to save."
+         redirect_to [@topic]
+       end
+
      elsif params[:post_id].present?
        # commeont on a post
-     end
-     @topic = Topic.find(params[:topic_id])
-     comment = @topic.comments.new(comment_params)
-     comment.user = current_user
+       @post = Post.find(params[:post_id])
+       comment = @post.comments.new(comment_params)
+       comment.user = current_user
 
-     if comment.save
-       flash[:notice] = "Comment saved successfully."
- # #12
-       redirect_to [@topic]
-     else
-       flash[:alert] = "Comment failed to save."
- # #13
-       redirect_to [@topic]
+       if comment.save
+         flash[:notice] = "Comment saved successfully."
+   # #12
+         redirect_to [@post.topic, @post]
+       else
+         flash[:alert] = "Comment failed to save."
+   # #13
+         redirect_to [@post.topic, @post]
+       end
      end
    end
 
