387c787d8ad2738f3a1997518fe3b9f2f79cb326
Geoff Miles <miles.geoff@gmail.com>
2016-08-07 17:44:38 -0400

    Assignment 42 schema updated, error in comments controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160730182637) do
+ActiveRecord::Schema.define(version: 20160723194512) do
 
   create_table "adverisements", force: :cascade do |t|
     t.string   "title"
@@ -24,12 +24,14 @@ ActiveRecord::Schema.define(version: 20160730182637) do
   create_table "comments", force: :cascade do |t|
     t.text     "body"
     t.integer  "post_id"
+    t.integer  "topic_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+  add_index "comments", ["topic_id"], name: "index_comments_on_topic_id"
   add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "favorites", force: :cascade do |t|
@@ -98,7 +100,6 @@ ActiveRecord::Schema.define(version: 20160730182637) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "role"
-    t.string   "auth_token"
   end
 
   create_table "votes", force: :cascade do |t|
